af5f71ea4756dace7004f7342c162b9de92e7ca2
hromerov <61013830+hromerov@users.noreply.github.com>
2020-03-31 07:05:19 +0100

    Initial commit

---
+++ mod/header.php
@@ -0,0 +1,28 @@
+<?php
+    print("
+    <header>
+        <div class=\"container\">
+            <div class=\"row\">
+                <div class=\"col-md-6 align-items-center\">
+                    <img class =\"logo\" src=\"images/cabildo.png\" alt=\"Cabildo de Gran Canaria\">
+                </div>
+                <div class=\"col-md-6 RRSS\">
+                    <ul class=\"social-header list-inline text-xs-right\">
+                        <li class=\"list-inline-item\">
+                            <a href=\"#\">
+                                <img class=\"logo-RRSS\" src=\"images/inst.png\" alt=\"Instagram\">
+                            </a>
+                        </li>
+                        <li class=\"list-inline-item\">
+                            <img class=\"logo-RRSS\" src=\"images/fb.png\" alt=\"Facebook\">
+                        </li>
+                        <li class=\"list-inline-item\">
+                            <img class=\"logo-RRSS\" src=\"images/tw.png\" alt=\"Twitter\">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>"
+);
+?>
